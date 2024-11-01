92106050e565fd2f93ffb47c8926e4c3419095e5
VishwasBJ <vishwasjai2004@gmail.com>
2024-11-01 17:19:37 +0530

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+function getWeather(){
+    const apiKey='YOUR-API-KEY';
+    const city= document.getElementById('city').value;
+
+    if(!city){
+        alert('Please Enter a city');
+        return;
+        
+    }
+}
